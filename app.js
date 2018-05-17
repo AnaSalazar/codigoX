ae8c3aab652ccac52c47da3739915098c5ae8da6
AnaSalazar <anastephany.salazarguzman@gmail.com>
2018-05-17 11:40:14 -0500

    Agrega las funcionaes para la conversion de pesos

--- app.js
+++ app.js
@@ -7,12 +7,33 @@ boton.addEventListener("click", cuestionario);
 function cuestionario() {
   var nombre = prompt("Hola ¿Cómo te llamas?");
   var peso = parseInt(prompt("Que lindo nombre. ¿Cuál es tu peso?"));
-  var animal = prompt("¿Con qué animalito quieres hacer la conversión? \n Selecciona 1-Gato 2-Perro 3-Conejo");
+  var eleccion = parseInt(prompt("¿Con qué animalito quieres hacer la conversión? \n Selecciona 1-Gato 2-Perro 3-Conejo"));
 
-  sacarResultado(nombre, peso, animal);
+  sacarResultado(nombre, peso, eleccion);
 }
 
-function sacarResultado(nombre, peso, animal) {
-  var resultado = peso / gato;
-  console.log(nombre + "Tu peso es equivalente a " + resultado);
-}
+function sacarResultado(nombre, peso, eleccion) {
+  var resultado = 0;
+  var respuesta = "";
+
+  switch (eleccion) {
+    case 1:
+      resultado = peso / gato;
+      respuesta = nombre + " tu peso equivale a " + resultado + " gatos."
+      break;
+    case 2:
+      resultado = peso / perro;
+      respuesta = nombre + " tu peso equivale a " + resultado + " perros."
+      break;
+    case 3:
+      resultado = peso / conejo;
+      respuesta = nombre + " tu peso equivale a " + resultado + " conejos."
+      break;
+  default:
+    resultado = 0;
+    respuesta = "Metiste datos incorrectos, intenta de nuevo";
+  }
+
+  var respuestaHtml = document.getElementById("resultado");
+  respuestaHtml.innerHTML = respuesta;
+};
